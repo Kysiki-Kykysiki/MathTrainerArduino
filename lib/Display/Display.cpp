6b8cfca5e85c7aa89b5decc6ed0cae67dacef193
Kysb <kirill.veber.81@mail.ru>
2026-06-19 16:47:41 +0700

    декомпозиция дисплея

---
+++ lib/Display/Display.cpp
@@ -0,0 +1,48 @@
+#include "Display.h"
+
+
+Display::Display(byte address, byte c, byte r): lcd(address, c,r), cols(c), rows(r){}
+void Display::begin(){
+    lcd.init();
+    lcd.backlight();    
+}
+void Display::showWelcome(){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Math TrainerV1.0");
+    lcd.setCursor(0, 1);
+    lcd.print("Press any key...");
+}
+void Display::showResult(bool correct, int correctAnswer, int score){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    if (correct) {
+        lcd.print("Pravilno :)");
+    } else {
+        lcd.print("!Pravilno :( ");
+        lcd.print(correctAnswer);
+    }
+    
+    lcd.setCursor(0, 1);
+    lcd.print("Schet: ");
+    lcd.print(score);
+}
+void Display::showInput(const String& input){
+    lcd.setCursor(7, 1);
+    lcd.print("         "); 
+    lcd.setCursor(7, 1);
+    lcd.print(input);
+}
+void Display::showQuestion(const Question& q){
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print(q.a);
+    lcd.print(" ");
+    lcd.print(q.op);
+    lcd.print(" ");
+    lcd.print(q.b);
+    lcd.print(" = ?");
+
+    lcd.setCursor(0, 1);
+    lcd.print("Otvet: ");
+}
